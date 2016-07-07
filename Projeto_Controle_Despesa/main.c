e763be4400fdddbbc82262dea7b27bf36e9ee6df
TauanBinato <tauanflores@gmail.com>
2016-07-06 23:15:29 -0300

    Initial Commit

---
+++ Projeto_Controle_Despesa/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Projeto_Controle_Despesa
+//
+//  Created by Tauan Flores on 7/6/16.
+//  Copyright © 2016 Astral Chest. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
